175530d4f3626703db6c71bd01f248ac22061cd9
xcheche <chekwubechukwu23@gmail.com>
2024-03-09 12:27:26 +0100

    blog etc -almost done

--- portfolio/models.py
+++ portfolio/models.py
@@ -10,4 +10,7 @@ class Project(models.Model):
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
+    def __str__(self):
+        return self.title
+
     
