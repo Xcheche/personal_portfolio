78478e24450760ac1be1f9d7da2f899db8384a4a
xcheche <chekwubechukwu23@gmail.com>
2024-03-11 18:33:43 +0100

    contact form added

--- portfolio/static/portfolio/scroll-to-top.js
+++ portfolio/static/portfolio/scroll-to-top.js
@@ -16,3 +16,29 @@ scrollToTopBtn.addEventListener('click', () => {
     behavior: 'smooth'
   });
 });
+
+// form submision
+
+
+
+function submitForm(event) {
+  event.preventDefault();
+
+  // Assuming the form submission is successful
+  // You can add your own form submission logic here if needed
+
+  // Show the success message
+  var successMessage = document.getElementById("success-message");
+  successMessage.innerText = "Sent successfully!";
+  successMessage.style.display = "block";
+
+  // Set a timeout to hide the success message after 5 seconds
+  setTimeout(function () {
+    successMessage.style.display = "none";
+  }, 5000);
+
+  // Reset the form fields (optional)
+  event.target.reset();
+
+  return false;
+}
